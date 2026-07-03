784678771f27d8f926ed6b90046acc68fe1564f5
christopherandev <christopherandradebr@gmail.com>
2026-07-02 21:13:36 -0300

    melhorias de layout e novos relógios

--- scripts/clocks.js
+++ scripts/clocks.js
@@ -4,7 +4,7 @@ let     DiffTimeZone = { hour: 0, min: 0};
 let     Events;
 
 const StrWeek = [ "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" ];
- 
+const StrMonth = [ "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" ];
 window.onload = async () =>
 {
     const user_tz = GetAppTimerZone() * (-1);
@@ -217,6 +217,56 @@ function OnAppUpdate()
 
                 break;
             }
+
+            case "date":
+            {
+                const now = new Date(skydate);
+                const next_date = new Date(schedule.isodate);
+                
+                next_date.setUTCHours(next_date.getUTCHours() + DiffTimeZone.hour);
+                next_date.setUTCMinutes(next_date.getUTCMinutes() + DiffTimeZone.min);
+
+                let remaining = (next_date.getTime() - now.getTime());
+
+                console.log(next_date.getUTCHours(), now.getUTCHours());
+                let str;
+                
+                if(remaining < 604800 * 1000)
+                {
+                    const weekday = StrWeek[next_date.getUTCDay()];
+                    const hour  = String(next_date.getUTCHours()).padStart(2, '0');
+                    const min   = String(next_date.getUTCMinutes()).padStart(2, '0');
+                    const sec   = String(next_date.getUTCSeconds()).padStart(2, '0');
+                    str = `${ weekday } às ${ hour }:${ min }:${ sec }`;
+                }
+
+                else
+                {
+                    const day   = String(next_date.getUTCDate()).padStart(2, '0');
+                    const month = String(next_date.getUTCMonth() + 1).padStart(2, '0');
+                    const year  = String(next_date.getUTCFullYear()).padStart(4, '0');
+                    const hour  = String(next_date.getUTCHours()).padStart(2, '0');
+                    const min   = String(next_date.getUTCMinutes()).padStart(2, '0');
+                    const sec   = String(next_date.getUTCSeconds()).padStart(2, '0');
+                    str = `${ day }/${ month }/${ year } às ${ hour }:${ min }:${ sec }`;
+                }
+
+                document.getElementById(event.element.event).innerText = str;
+
+                const timestamp = Math.floor(remaining / 1000);
+
+                const days      = Math.floor(timestamp / 86400);
+                const hours     = Math.floor(timestamp / 3600);
+                const minutes   = Math.floor((timestamp % 3600) / 60);
+                const seconds   = timestamp % 60;
+
+                const doc = `${String(days).padStart(2, '0')} dias ` + `${String(hours).padStart(2, '0')} h ` + `${String(minutes).padStart(2, '0')} m ` + `${String(seconds).padStart(2, '0')} s`
+
+                document.getElementById(event.element.diff).innerText = doc;
+
+                break;
+            }
+
             default:
     
         }
